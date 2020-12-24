688eaf640f2213d9ae24958befddc0e1d580e46a
Sandip Ghosh <72227918+sandipghosh18@users.noreply.github.com>
2020-12-24 23:46:57 +0530

    Create GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,21 @@
+package snake;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame 
+{
+	GameFrame()
+	{
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+
+}
